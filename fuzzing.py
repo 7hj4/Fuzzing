75dd6e9e3c137d5ccb9632118d6d22563dc0b8ac
yousef <54207974+7hj4@users.noreply.github.com>
2020-09-03 03:49:22 +0300

    Create fuzzing.py

---
+++ fuzzing.py
@@ -0,0 +1,48 @@
+import requests
+import time
+
+
+logo = '''
+
+                 ########################################
+
+
+                 ------- Devloper by Yousef -----------
+                         twitter : y0usef_11
+
+                      	1 - web site directory
+	                2 - (soon) Api directory
+
+                 ########################################
+
+'''
+
+print logo
+
+def main():
+    # The number selection by the user
+    options = input("Please choose a number --> " )
+    if options == 1 :
+        api_directory()
+    if options == 2 :
+        print "noting :( "
+    else :
+        print "to noting :( "
+
+def api_directory():
+     url = raw_input("Url Input --> ")
+     file_text = raw_input("Input file text --> ")
+     file = open("urls_api.txt","w")
+     read = open(file_text,"r")
+     for line in read :
+         req = requests.get(url + str(line))
+         if req.status_code != 404:
+             file.write(str(req.request.url)+ "\n")
+             print req.request.url, " == 200 Ok"
+         else:
+             print " Not Found "
+
+
+  
+if __name__ == "__main__":
+    main()
